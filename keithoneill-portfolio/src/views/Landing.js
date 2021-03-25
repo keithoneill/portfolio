f24c6eb2a4640bfc6921821d8f42df401925ce18
keithoneill <koneill1@student.fullsail.edu>
2021-03-24 21:58:21 -0400

    Created mp cert view

---
+++ keithoneill-portfolio/src/views/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Hero from '../components/Hero';
+import Projects from '../components/Projects';
+
+function Landing() {
+  return (
+    <div>
+      <Hero />
+      <Projects />
+    </div>
+  );
+}
+
+export default Landing;
