a7f00af89e81f4183963923f8da3cb6899f6c413
keithoneill <koneill1@student.fullsail.edu>
2021-03-25 20:07:33 -0400

    Changed bg

--- keithoneill-portfolio/src/components/Hero.js
+++ keithoneill-portfolio/src/components/Hero.js
@@ -3,24 +3,22 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import { Typography } from '@material-ui/core';
 import HeroLayout from './HeroLayout';
+import Background from '../images/gridpapersmall.jpg'
 
-const backgroundImage =
-  'https://images.unsplash.com/photo-1598940603846-a1edd0ef2574?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTJ8fGJhY2tncm91bmQlMjBncmVlbnxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60';
+// const backgroundImage =
+//   'https://images.unsplash.com/photo-1616593772450-6220bc809944?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTN8fGdyaWQlMjBwYXBlcnxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60';
 
 const styles = (theme) => ({
   background: {
-    backgroundImage: `url(${backgroundImage})`,
-    backgroundColor: '#ddd',
+    backgroundImage: `url(${Background})`,
+    backgroundColor: '#E9C46A',
     backgroundPosition: 'center',
-  },
-  button: {
-    minWidth: 200,
   },
   h5: {
     marginBottom: theme.spacing(4),
     marginTop: theme.spacing(4),
     [theme.breakpoints.up('sm')]: {
-      marginTop: theme.spacing(10),
+      marginTop: theme.spacing(8),
     },
   },
   more: {
@@ -34,8 +32,8 @@ function Hero(props) {
   return (
     <HeroLayout backgroundClassName={classes.background}>
       {/* Increase the network loading priority of the background image. */}
-      <img style={{ display: 'none' }} src={backgroundImage} alt="increase priority" />
-      <Typography color="inherit" align="center" variant="h2" marked="center">
+      <img style={{ display: 'none' }} src={Background} alt="increase priority" />
+      <Typography color="inherit" align="center" variant="h2" marked="center" style={{ marginTop: '30px' }}>
         Keith O'Neill
       </Typography>
       <Typography color="inherit" align="center" variant="h5" className={classes.h5}>
