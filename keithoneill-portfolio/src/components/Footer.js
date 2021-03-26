fa95a756cb3d5f38e3b6a799c2cd89b46713fbf3
keithoneill <koneill1@student.fullsail.edu>
2021-03-25 21:54:23 -0400

    Restyled footer

--- keithoneill-portfolio/src/components/Footer.js
+++ keithoneill-portfolio/src/components/Footer.js
@@ -1,44 +1,87 @@
 import React from 'react';
+import { CssBaseline, Typography, Container, Link, List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import { Grid, Link, Container, Typography } from '@material-ui/core'
+import ReceiptIcon from '@material-ui/icons/Receipt';
+import EmailIcon from '@material-ui/icons/Email';
+import ContactMailIcon from '@material-ui/icons/ContactMail';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
-    backgroundColor: '#264653',
+    flexDirection: 'column',
+    minHeight: '100vh',
   },
-  container: {
+  main: {
     marginTop: theme.spacing(8),
-    marginBottom: theme.spacing(8),
-    display: 'flex',
+    marginBottom: theme.spacing(2),
   },
-  icon: {
+  footer: {
+    padding: theme.spacing(3, 2),
+    marginTop: 'auto',
+    backgroundColor: '#264653',
+    textAlign: 'center',
     color: '#ddd',
-    width: 50,
-    height: 78,
-  }
+  },
+  links: {
+    color: '#264653',
+    '&:hover': {
+      backgroundColor: 'rgba(51, 170, 51, .1) ',
+    },
+  },
 }));
 
 export default function Footer() {
   const classes = useStyles();
 
   return (
-    <Typography component="footer" className={classes.root}>
-      <Container className={classes.container}>
-        <Grid container spacing={5}>
-          <Grid item xs={6} sm={4} md={2}>
-            <Typography variant="h4" gutterBottom>
-              Contact:
-            </Typography>
-            <Typography variant="h5" >
-              <Link href="mailto:keithaoneill83@gmail.com" style={{ textDecoration: 'none', color: '#ddd' }}>keithaoneill83@gmail.com</Link>
-              <Link href="/resume" style={{ textDecoration: 'none', color: '#ddd', marginLeft: '80px' }}>Resume</Link>
-              <Link href="/biography" style={{ textDecoration: 'none', color: '#ddd', marginLeft: '80px' }}>Biography</Link>
-              <Link href="https://www.linkedin.com/in/keith-o-neill-280179133/" style={{ textDecoration: 'none', color: '#ddd', marginLeft: '80px' }}>LinkedIn</Link>
-            </Typography>
-          </Grid>
-        </Grid>
+    <div className={classes.root}>
+      <CssBaseline />
+      <Container component="main" className={classes.main} maxWidth="sm">
+        <Typography variant="h2" component="h1" gutterBottom>
+          Contact
+        </Typography>
+          <List>
+            <Link href="mailto:keithaoneill83@gmail.com" style={{ textDecoration: 'none' }} >
+              <ListItem>
+                <ListItemIcon>
+                  <EmailIcon />
+                </ListItemIcon>
+                <ListItemText primary={<Typography variant='h5' className={classes.links}>keithaoneill83@gmail.com</Typography>}/>
+              </ListItem>
+            </Link>
+            <Link href="/resume" style={{ textDecoration: 'none' }} >
+              <ListItem>
+                <ListItemIcon>
+                  <ReceiptIcon />
+                </ListItemIcon>
+                <ListItemText primary={<Typography variant='h5' className={classes.links}>Resume</Typography>}/>
+              </ListItem>
+            </Link>
+            <Link href="/contact" style={{ textDecoration: 'none' }} >
+              <ListItem>
+                <ListItemIcon>
+                  <ContactMailIcon />
+                </ListItemIcon>
+                <ListItemText primary={<Typography variant='h5' className={classes.links}>Contact</Typography>}/>
+              </ListItem>
+            </Link>
+            <Link href="https://www.linkedin.com/in/keith-o-neill-280179133/" style={{ textDecoration: 'none' }} >
+              <ListItem>
+                <ListItemIcon>
+                  <LinkedInIcon />
+                </ListItemIcon>
+                <ListItemText primary={<Typography variant='h5' className={classes.links}>LinkedIn</Typography>}/>
+              </ListItem>
+            </Link>
+          </List>
       </Container>
-    </Typography>
+      <footer className={classes.footer}>
+        <Container maxWidth="sm">
+          <Typography variant="body1">Keith O'Neill - Instructional Designer</Typography>
+        </Container>
+      </footer>
+    </div>
   );
 }
