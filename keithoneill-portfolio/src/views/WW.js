5744268c2fb0e11bd39d1dd5ed75d24cfa782cbb
keithoneill <koneill1@student.fullsail.edu>
2021-03-26 19:58:20 -0400

    Fixed spelling

--- keithoneill-portfolio/src/views/WW.js
+++ keithoneill-portfolio/src/views/WW.js
@@ -13,7 +13,7 @@ function WW() {
       image02 = {Image02}
       gif2 = {Gif}
       job = 'Design and Development'
-      description = 'For my Capstone Project in the Web Design and Development Degree, created a web application to assist people in finding what to watch called WhatchuWatchin. I created the front end in React and Redux. I also created the back end storing users information in a database via sequelize. The application allows users to choose shows or movies to watch with a tinder styles swipe. Liked videos are added to the users watchlist with future updates incorporating linked accounts with shared watchlist. I beleive applications like this can be incoporated in a learning environment for multiple select questions or group or team assigning.'
+      description = 'For my Capstone Project in the Web Design and Development Degree, I created a web application to assist people in finding what to watch called WhatchuWatchin. I created the front end in React and Redux. I also created the back end storing users information in a database via sequelize. The application allows users to choose shows or movies to watch with a tinder styles swipe. Liked videos are added to the users watchlist with future updates incorporating linked accounts with shared watchlist. I beleive applications like this can be incoporated in a learning environment for multiple select questions or group or team assigning.'
       />
     </div>
   );
